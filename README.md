a5c605f68f9b61e14de495e56de952f4db2bd938
Daniele Gadler <daniele.gadler@siag.it>
2020-03-15 20:55:30 +0100

    Added code and basic plot for totale casi

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# time-series-covid-19
-Time series algorithms applied to covid-19 dataset
+# Time-Series Covid-19 spread in Italy
+Time series algorithms applied to covid-19 dataset spread in Italy
+based on the dataset by 'Protezione Civile':
+https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-json/dpc-covid19-ita-andamento-nazionale.json
+
+## Totale casi:
+
+[!Co](./covid_totale_casi.png)
+
+
---
+++ virus.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+"""
+Daniele Gadler
+Coronavirus time series prediction
+based on ARIMA model
+Updated to 15.03.2020
+"""
+
+import json
+import numpy as np
+import matplotlib.pyplot as plt
+from statsmodels.tsa.arima_model import ARIMA
+from statsmodels.tsa.stattools import adfuller
+import pandas as pd
+from pylab import rcParams
+rcParams['figure.figsize'] = 10, 10
+
+FILE_PATH="dpc-covid19-ita-andamento-nazionale.json"
+
+
+with open(FILE_PATH, 'r') as f:
+    json_virus = json.load(f)
+    
+#Let's get all the dates in each dictionary
+list_dates = [dict_date["data"] for dict_date in json_virus]
+
+#Let's get all the 'totale casi' in each dictionary
+list_totale_casi = [dict_date["totale_casi"] for dict_date in json_virus]
+
+#Let's compute a diff 
+
+list_diff_casi = []
+for x, y in zip(list_totale_casi[0::], list_totale_casi[1::]): 
+    list_diff_casi.append(y-x) 
+    
+list_diff_casi.insert(0, 0)
+
+plt.plot(list_dates, list_diff_casi)
+plt.title("Nuovi casi di Covid-19 in Italia")
+plt.xticks(rotation=90)
+
+plt.plot(list_dates, list_totale_casi)
+plt.title("Numero di casi di Covid-19 in Italia")
+plt.xticks(rotation=90)
+
+#Let's take a look at the data
+print(list_dates)
+print(list_totale_casi)
+
+#Rolling statistics, the time window is 1, since there is no "cyclic behaviour here"
+df_totale_casi = pd.DataFrame({"totale_casi" : list_totale_casi})
+df_totale_casi.index=list_dates
+df_totale_casi.index = pd.DatetimeIndex(df_totale_casi.index).to_period('D')
+    
+rolling_mean = df_totale_casi["totale_casi"].rolling(window=3).mean()
+
+plt.xticks(rotation=90)
+#Let's plot the trend every 3 days
+plt.plot(list_dates, rolling_mean, list_totale_casi)
+
+#Let's perform a test for stationarity with Dicker-Fuller
+df_test = adfuller(df_totale_casi["totale_casi"], autolag="AIC")
+
+df_output = pd.Series(df_test[0:4], index=["Test statistic", "p-value", "#Lags used", "Num. of obs used"])
+
+#Cannot reject the null hypothesis, the data is NOT stationary!
+print(df_output)
+
+#ARIMA model forforecasting upcoming values
+#Parameters for order: p-value, differentiating, Q-value
+model = ARIMA(df_totale_casi, order=(2, 1, 0))
+results_ARIMA = model.fit(disp=-1)
+
+fig = results_ARIMA.plot_predict(1, 30)
+fig.suptitle("Numero totale casi di Covid-19 in Italia")
+
+   
+
+
+
+
+
+
